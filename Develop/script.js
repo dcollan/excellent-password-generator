12f80c0bc2863a32abf22a643a117a18f9add95b
Danny Collan <collan97@gmail.com>
2023-08-20 18:38:31 -0700

    Added prompt dialogs and for-loops for condition satisfying

--- Develop/script.js
+++ Develop/script.js
@@ -25,3 +25,47 @@ var acceptLowerCase;
 var acceptUpperCase;
 var acceptSpecialChar;
 var selected;
+
+// Initialized function to obtain user input using provided 'generatePassword()' method
+function generatePassword() {
+// Referred to Stack Overflow source for how to display prompt: https://stackoverflow.com/questions/37287093/starting-a-javascript-prompt-after-a-button-is-clicked
+  SizeOfPassword = prompt("Hi, there! Please enter a value between 8 and 128 for the size of your password");
+  console.log("Size of Password " + SizeOfPassword);
+  
+// Set up boundaries on user needing to input value in between 8 and 128 (inclusively)
+// If value is outside the range, program will continue to loop until condition is satisfied
+  for(var i = 0; i < number.length; i++) {
+    if (SizeOfPassword < 8 || SizeOfPassword > 128) {
+      SizeOfPassword = prompt("Um. Please try again. Enter a value between 8 and 128 for your password");
+      console.log("Size of Password " + SizeOfPassword);
+    }
+  } 
+
+// Set up if-statement regarding if value is within the range
+// User will receive further prompts on if password will have lowercase letters, uppercase letters, numbers, and/or special characters
+  if(SizeOfPassword >= 8 || SizeOfPassword <= 128) { 
+    acceptLowerCase = confirm("Thanks. Will there be any lowercase letters?");
+    console.log("Lower case " + acceptLowerCase);
+    acceptUpperCase = confirm("You got it. Any uppercase letters?");
+    console.log("Upper case " + acceptUpperCase);
+    acceptNumericValue = confirm("Okay. Numbers then, by any chance?");
+    console.log("Number " + acceptNumericValue);
+    acceptSpecialChar = confirm("Great, last one. Any special characters?");
+    console.log("Special Character " + acceptSpecialChar);
+  }
+
+  // Set up for if no options were selected, so that program will loop until at least one has been selected
+  for(var i = 0; i < number.length; i++) {
+    if (acceptLowerCase === false && acceptUpperCase === false && acceptNumericValue === false && acceptSpecialChar === false) {
+      selected = alert("You...didn't select one. Please choose at least one!");
+      acceptLowerCase = confirm("Let's try again. Any lowercase letters?");
+      console.log("Lower case " + acceptLowerCase);
+      acceptUpperCase = confirm("Any uppercase letters?");
+      console.log("Upper case " + acceptUpperCase);
+      acceptNumericValue = confirm("How about numbers?");
+      console.log("Number " + acceptNumericValue);
+      acceptSpecialChar = confirm("Special characters?");
+      console.log("Special Character " + acceptSpecialChar);
+    } 
+  }
+}
